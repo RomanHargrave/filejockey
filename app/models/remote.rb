2f08fbae92cd1b1ffb5f1353a39e53c28221d2b3
Roman Hargrave <roman@hargrave.info>
2019-09-09 18:47:05 -0500

    Add relations for remote and transmission

--- app/models/remote.rb
+++ app/models/remote.rb
@@ -1,2 +1,6 @@
 class Remote < ApplicationRecord
+
+  has_many :jobs
+  has_many :transmissions, through: :jobs
+
 end
--- app/models/transmission.rb
+++ app/models/transmission.rb
@@ -1,5 +1,13 @@
 # FileJockey transmission log record
 # (C) 2019 Roman Hargrave
 class Transmission < ActiveRecord::Base
-   
+   belongs_to :job
+
+   def remote
+      self.job.remote
+   end
+
+   def repository
+      self.job.repository
+   end
 end
