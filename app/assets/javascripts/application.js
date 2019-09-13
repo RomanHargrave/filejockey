135ed2fc7f4f062c5dd63100d044c9ecaa5dcede
Roman Hargrave <roman@hargrave.info>
2019-09-13 16:30:33 -0500

    Add jquery to make foundation javascript work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,7 @@
+//= require jquery3
 //= require foundation
 $(document).on('turbolinks:load', function() {
   $(function() { $(document).foundation(); });
 });
+
+$(function(){ $(document).foundation(); });
