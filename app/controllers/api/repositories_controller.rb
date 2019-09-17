089291039178328622c7cf51aa2b40d75c22fcf1
Roman Hargrave <roman@hargrave.info>
2019-09-17 16:15:05 -0500

    Add basic CRUD for Repository

--- app/controllers/api/repositories_controller.rb
+++ app/controllers/api/repositories_controller.rb
@@ -11,4 +11,25 @@ class Api::RepositoriesController < ApplicationController
     render json: result.map {|r| r.as_json}
   end
 
+  def create
+    rep  = JSON.parse(params[:data])
+    new  = Repository.new(rep)
+    render json: new.as_json
+  end
+
+  def update
+    rep  = JSON.parse(params[:data])
+
+    if rep.include? :id
+      Repository.update(rep[:id], rep)
+      render json: Repository.get(rep[:id]).as_json
+    else
+      render status: 404
+    end
+  end
+
+  def delete
+    Repository.delete(params[:id])
+  end
+
 end
