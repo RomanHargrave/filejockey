94910bf6c0e0edeaa85da80590aaf2810785aec4
Roman Hargrave <roman@hargrave.info>
2019-09-17 17:09:18 -0500

    Initial work on Repository list component

--- app/javascript/components/repositories/Repositories.js
+++ app/javascript/components/repositories/Repositories.js
@@ -1,8 +1,16 @@
 import React from "react"
 
+import Client from "api/FileRouterClient"
+
+import MaterialTableIcons from "components/MaterialTableIcons"
+import MaterialTable from "material-table"
+
 export default function Repositories(props) {
+  const {client} = props;
+
   return (
-    <React.Fragment>
-    </React.Fragment>
+    <MaterialTable
+      icons={MaterialTableIcons}
+      title="Configured Repositories" />
   );
 }
