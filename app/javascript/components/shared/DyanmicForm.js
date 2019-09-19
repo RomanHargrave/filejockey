927807d06142193a4eb927e3440bada582139b4a
Roman Hargrave <roman@hargrave.info>
2019-09-19 09:30:46 -0500

    (Checkpoint) add DynamicForm component

---
+++ app/javascript/components/shared/DyanmicForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropType from 'prop-types'
+
+import {
+  Button,
+  Grid,
+  FormControl,
+  InputLabel,
+  Input,
+  TextField
+} from '@material-ui/core'
+
+export default function DynamicForm({ fields, onSubmit }) {
+  return (<React.Fragment />);
+}
