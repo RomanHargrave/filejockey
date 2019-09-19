9ed9c00cce66b606c7a0851b1e36f4b9e85eafe8
Roman Hargrave <roman@hargrave.info>
2019-09-19 09:29:57 -0500

    Add FAB to Repository List

--- app/javascript/components/repositories/Repositories.js
+++ app/javascript/components/repositories/Repositories.js
@@ -1,13 +1,47 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import { Fab } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Add as AddIcon } from '@material-ui/icons'
+
 import GenerateRoutes from 'components/shared/GenerateRoutes'
 
 // Route components
 import RepositoryList from './RepositoryList'
 
+/**
+ * Offers via FAB the oppourtunity to create a new repo
+ */
+function AddRepository({ apiClient }) {
+  const addRepoStyle = makeStyles((theme) => ({
+    fab: {
+      position: 'absolute',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+      backgroundColor: theme.secondary.light
+    },
+    fabIcon: {
+      color: theme.secondary.contrastText
+    }
+  }));
+
+  const css = addRepoStyle();
+
+  const {open, setOpen} = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <Fab aria-label='Add' className={css.fab}>
+        <AddIcon className={css.fabIcon} />
+      </Fab>
+    </React.Fragment>
+  );
+}
+
 const routes = [
-  { path: '', exact: true, component: RepositoryList, key: 'display-repo-list' }
+  { path: '', exact: true, component: RepositoryList, key: 'display-repo-list'  },
+  { path: '', exact: true, component: AddRepository,  key: 'add-repo-available' }
 ];
 
 export default function Repositories({ apiClient, match }) {
