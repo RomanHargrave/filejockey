35e9ad1f4a99b4097b412de8dcdc276a248cb7e6
Roman Hargrave <roman@hargrave.info>
2019-09-19 10:29:55 -0500

    Add envelope processing to FileRouterClient axios instance

--- app/javascript/api/FileRouterClient.js
+++ app/javascript/api/FileRouterClient.js
@@ -9,6 +9,35 @@ import { RepositoryResource, Repository } from './Repository'
 
 import ResultPage from './ResultPage'
 
+/**
+ * Processes API response envelopes.
+ * For status == 'error', an exception is raised,
+ * otherwise, .data is extracted to .
+ */
+function processResponseEnvelope(envelope_raw) {
+  try {
+    const envelope = JSON.parse(envelope_raw);
+
+    if (envelope.status !== undefined) {
+      if (envelope.status === 'error') {
+        throw `Error${' ' + envelope.status || ''}: ${envelope.message || 'The endpoint responded with an error condition but did not provide a message'}`;
+      } else {
+        return (envelope.data !== undefined) ? envelope.data : {};
+      }
+    } else {
+      // If a status field is not present, this is probably a bare response
+      return envelope;
+    }
+  } catch (e) {
+    // If the response data wasn't valid JSON, just pass it along
+    if (e.name === "SyntaxError") {
+      return envelope_raw;
+    } else {
+      throw e;
+    }
+  }
+}
+
 export default class FileRouterClient {
   constructor(params) {
     const { baseURL } = params || {};
@@ -17,7 +46,8 @@ export default class FileRouterClient {
     this.apiVersion = FileRouterClient.apiVersion;
     this.resources  = {};
     this.axios      = axios.create({
-      baseURL: this.apiBase
+      baseURL: this.apiBase,
+      transformResponse: [ processResponseEnvelope ]
     });
 
     this.addResource(RepositoryResource);
