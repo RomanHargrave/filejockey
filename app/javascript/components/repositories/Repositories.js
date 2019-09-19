6a03c90bbe8afcec8f7863d3477a84d98e27eac4
Roman Hargrave <roman@hargrave.info>
2019-09-19 17:09:29 -0500

    Replace discreet use of FAB with CornerFAB

--- app/javascript/components/repositories/Repositories.js
+++ app/javascript/components/repositories/Repositories.js
@@ -1,10 +1,9 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import { Fab } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import { Add as AddIcon } from '@material-ui/icons'
 
+import CornerFAB from 'components/shared/CornerFAB'
 import GenerateRoutes from 'components/shared/GenerateRoutes'
 
 // Route components
@@ -14,28 +13,10 @@ import RepositoryList from './RepositoryList'
  * Offers via FAB the oppourtunity to create a new repo
  */
 function AddRepository({ apiClient }) {
-  const addRepoStyle = makeStyles((theme) => ({
-    fab: {
-      position: 'absolute',
-      bottom: theme.spacing(2),
-      right: theme.spacing(2),
-      backgroundColor: theme.secondary.light
-    },
-    fabIcon: {
-      color: theme.secondary.contrastText
-    }
-  }));
-
-  const css = addRepoStyle();
-
-  const {open, setOpen} = React.useState(false);
-
   return (
-    <React.Fragment>
-      <Fab aria-label='Add' className={css.fab}>
-        <AddIcon className={css.fabIcon} />
-      </Fab>
-    </React.Fragment>
+    <CornerFAB>
+      <AddIcon />
+    </CornerFAB>
   );
 }
 
